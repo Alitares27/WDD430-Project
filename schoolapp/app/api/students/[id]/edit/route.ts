c65ddb89be27b9c36e4245b26a14b6b2516778aa
Alitares27 <aldair2795@hotmail.com>
2025-06-09 20:10:24 -0300

    refactor: Simplify PUT function by removing params argument and enhancing ID extraction from URL

--- schoolapp/app/api/students/[id]/edit/route.ts
+++ schoolapp/app/api/students/[id]/edit/route.ts
@@ -3,25 +3,27 @@ import { getStudents } from '@/app/lib/data';
 import { updateStudent } from '@/app/lib/actions';
 import type { Student } from '@/app/lib/definitions';
 
-export async function PUT(
-    request: Request,
-    { params }: { params: { id: string } }
-) {
+export async function PUT(request: Request) {
     try {
-        const { id } = params;
-        const body = await request.json();
+        const url = new URL(request.url);
+        const id = url.pathname.split("/").pop(); 
 
+        if (!id) {
+            return NextResponse.json({ error: 'ID is required' }, { status: 400 });
+        }
+
+        const body = await request.json();
         const students: Student[] = await getStudents();
         const studentIndex = students.findIndex((s: Student) => String(s.id) === id);
 
         if (studentIndex === -1) {
             return NextResponse.json({ error: 'Student not found.' }, { status: 404 });
         }
+
         const updatedStudent: Student = { ...students[studentIndex], ...body };
         await updateStudent(updatedStudent);
         return NextResponse.json(updatedStudent);
-    } catch  {
+    } catch {
         return NextResponse.json({ error: 'Failed to update student.' }, { status: 500 });
     }
 }
-
